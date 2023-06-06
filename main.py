6ccbfff659b7cd16640b144149e47cc868c3b0c4
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2023-06-06 11:26:57 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,20 +6,23 @@ print(text)
 
 #TODO разобрать строки на отдельные слова 
 
-class Text_analyser:
-    def __init__(self, file_path, encoding):
+class TextAnalyser:
+    def __init__(self, file_path='text.txt', mode='r', encoding='UTF-8'):
         self.text = file_path
         self.encoding = encoding
         self.open_file()
         self.read_file()
         self.print_file()
+
     def open_file(self):
         self.text = open(self.text, 'r', encoding=self.encoding)
+
     def read_file(self):
         self.text = self.text.read()
+
     def print_file(self):
         print(self.text)
         
         
 
-test = Text_analyser('text.txt', 'UTF-8')
+test = TextAnalyser(mode='text.txt', encoding='UTF-8')
