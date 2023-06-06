a928c51717388ec0c5f673680ae4f0d5b0e584a7
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2023-06-06 10:35:45 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,18 @@
+'''
+content = open('test.txt', 'r', encoding='UTF-8')
+text = content.read()
+print(text)
+'''
+
+#TODO разобрать строки на отдельные слова 
+
+class Text_analyser:
+    def __init__(self, file_path, encoding):
+        self.text = file_path
+        self.encoding = encoding
+    def read_file(self):
+        text = open(self.text, 'r', encoding=self.encoding)
+        print(text = content.read())
+
+test = Text_analyser('text.txt', 'UTF-8')
+test.read_file()
