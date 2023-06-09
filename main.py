7224c4661fd03d4c7b451f0933bbd709f4173cf1
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2023-06-09 12:52:42 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,28 +1,29 @@
-'''
-content = open('test.txt', 'r', encoding='UTF-8')
-text = content.read()
-print(text)
-'''
-
 #TODO разобрать строки на отдельные слова 
 
 class TextAnalyser:
-    def __init__(self, file_path='text.txt', mode='r', encoding='UTF-8'):
+    def __init__(self, file_path=None, mode='r', encoding='UTF-8'):
+        if file_path is None:
+            raise Exception('Файл не указан')
         self.text = file_path
         self.encoding = encoding
-        self.open_file()
-        self.read_file()
-        self.print_file()
+        self.open_file(file_path)
+        self.print_file(file_path)
 
-    def open_file(self):
-        self.text = open(self.text, 'r', encoding=self.encoding)
+    def open_file(self, file_path):
+        try:
+            with open(self.text, 'r', encoding=self.encoding) as text:
+                self.text = text
+                self.text = self.text.read()
+                self.text = self.text.lower()
+        except FileNotFoundError:
+            raise Exception(f'Файл "{file_path}" не найден')
 
-    def read_file(self):
-        self.text = self.text.read()
-
-    def print_file(self):
-        print(self.text)
+    def print_file(self, file_path):
+        if not self.text:
+            raise Exception(f'Файл "{file_path}" пустой')
+        else:
+            print(self.text)
+    
         
-        
 
-test = TextAnalyser(mode='text.txt', encoding='UTF-8')
+test = TextAnalyser(file_path='empty.txt', encoding='UTF-8')
