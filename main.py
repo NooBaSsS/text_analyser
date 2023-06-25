aabe5b0a41b2f199776e470f5e748f4f0e969492
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2023-06-25 19:26:21 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -13,11 +13,20 @@ all_pos = [
     'CONJ',
     'PRCL',
     'INTJ',
+    None,
 ]
 
 
 class TextAnalyser:
-    def __init__(self, file_path=None, mode='r', encoding='UTF-8') -> None:
+    def __init__(self,
+                 file_path=None,
+                 mode='r',
+                 encoding='UTF-8',
+                 pos='NOUN',
+                 pos_1=None,
+                 pos_2=None,
+                 pos_3=None,
+                 ) -> None:
         if file_path is None:
             raise Exception('Файл не указан')
         self.text = file_path
@@ -25,7 +34,7 @@ class TextAnalyser:
         self.open_file(file_path)
         self.check_empty(file_path)
         self.prepare_text()
-        self.make_analysed_words('')
+        self.make_analysed_words(pos, pos_1, pos_2, pos_3)  # до 4 частей речи
         self.print_file()
 
     def open_file(self, file_path) -> None | NoReturn:
@@ -47,19 +56,54 @@ class TextAnalyser:
     def print_file(self) -> None:
         print(self.words)
         print(self.result)
+        if self.result_1:
+            print(self.result_1)
+        if self.result_1:
+            print(self.result_2)
+        if self.result_1:
+            print(self.result_3)
         print(f'длина {len(self.words)} слов')
 
-    def make_analysed_words(self, pos=['VERB']) -> None:
+    def make_analysed_words(self,
+                            pos='NOUN',
+                            pos_1=None,
+                            pos_2=None,
+                            pos_3=None
+                            ) -> None:
         if not self.words:
             raise Exception('Текста нет')
         if pos not in all_pos:
             raise Exception('Неправильная часть речи')
+        if pos_1 not in all_pos:
+            raise Exception('Неправильная часть речи')
+        if pos_2 not in all_pos:
+            raise Exception('Неправильная часть речи')
+        if pos_3 not in all_pos:
+            raise Exception('Неправильная часть речи')
         self.result = []
+        self.result_1 = []
+        self.result_2 = []
+        self.result_3 = []
         morph = pymorphy3.MorphAnalyzer()
         for word in self.words:
             parse = morph.parse(word)[0]
             if parse.tag.POS == pos:
                 self.result.append(morph.parse(word)[0].normal_form)
+
+        for word in self.words:
+            parse = morph.parse(word)[0]
+            if parse.tag.POS == pos_1:
+                self.result_1.append(morph.parse(word)[0].normal_form)
+
+        for word in self.words:
+            parse = morph.parse(word)[0]
+            if parse.tag.POS == pos_2:
+                self.result_2.append(morph.parse(word)[0].normal_form)
+
+        for word in self.words:
+            parse = morph.parse(word)[0]
+            if parse.tag.POS == pos_3:
+                self.result_3.append(morph.parse(word)[0].normal_form)
 
         '''
         self.parsed_word = self.parse[0]
